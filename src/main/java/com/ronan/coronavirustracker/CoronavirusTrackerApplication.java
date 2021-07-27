b8cb0798b28f7b3709e787275c8bb30d045de7e4
Ronan A. Behan <62386723+RonanBehan26@users.noreply.github.com>
2021-07-27 19:14:18 +0100

    Update CoronavirusTrackerApplication.java

--- src/main/java/com/ronan/coronavirustracker/CoronavirusTrackerApplication.java
+++ src/main/java/com/ronan/coronavirustracker/CoronavirusTrackerApplication.java
@@ -11,7 +11,6 @@ public class CoronavirusTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoronavirusTrackerApplication.class, args);
 
-		System.out.println("Hello World");
 	}
 
 }
